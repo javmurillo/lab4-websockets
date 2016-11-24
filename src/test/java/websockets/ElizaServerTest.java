6e147ed059500932f33850a843804f5d328297aa
Javier Murillo <jmurillo93@gmail.com>
2016-11-24 19:25:33 +0100

    ElizaServerTest fixed

--- src/test/java/websockets/ElizaServerTest.java
+++ src/test/java/websockets/ElizaServerTest.java
@@ -54,16 +54,19 @@ public class ElizaServerTest {
 	}
 
 	@Test(timeout = 1000)
-	@Ignore
 	public void onChat() throws DeploymentException, IOException, URISyntaxException, InterruptedException {
 		// COMPLETE ME!!
+        CountDownLatch latch = new CountDownLatch(5);
 		List<String> list = new ArrayList<>();
 		ClientEndpointConfig configuration = ClientEndpointConfig.Builder.create().build();
 		ClientManager client = ClientManager.createClient();
-		client.connectToServer(new ElizaEndpointToComplete(list), configuration, new URI("ws://localhost:8025/websockets/eliza"));
+		client.connectToServer(new ElizaEndpointToComplete(list, latch), configuration, new URI("ws://localhost:8025/websockets/eliza"));
 		// COMPLETE ME!!
+        latch.await();
 		// COMPLETE ME!!
+        assertEquals(5, list.size());
 		// COMPLETE ME!!
+        assertEquals("We were discussing you, not me.", list.get(3));
 	}
 
 	@After
@@ -92,16 +95,18 @@ public class ElizaServerTest {
     private static class ElizaEndpointToComplete extends Endpoint {
 
         private final List<String> list;
+        private final CountDownLatch latch;
 
-        ElizaEndpointToComplete(List<String> list) {
+        ElizaEndpointToComplete(List<String> list, CountDownLatch latch) {
             this.list = list;
+            this.latch = latch;
         }
 
         @Override
         public void onOpen(Session session, EndpointConfig config) {
 
             // COMPLETE ME!!!
-
+            session.getAsyncRemote().sendText("I think you are a robot");
             session.addMessageHandler(new ElizaMessageHandlerToComplete());
         }
 
@@ -111,6 +116,7 @@ public class ElizaServerTest {
             public void onMessage(String message) {
                 list.add(message);
                 // COMPLETE ME!!!
+                latch.countDown();
             }
         }
     }
